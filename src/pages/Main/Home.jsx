7892f5ec94ba2cdfd536056723f494f3d20c6ab8
Thun-2004 <66011276@kmitl.ac.th>
2024-09-23 21:00:08 +0700

    remove header from other pages

--- src/pages/Main/Home.jsx
+++ src/pages/Main/Home.jsx
@@ -4,11 +4,16 @@ import RestaurantCard from '../../components/RestaurantCard';
 import CarouselComponent from '../../components/CarouselComponent';
 import Banner from '../../components/Banner';
 import QueueCard from '../../components/QueueCard';
+import Header from '../../components/Header';
 import React from 'react';
 
 const Home = () => {
     return (
         <div className="p-7 flex">
+            <div className="fixed top-0 right-0 left-0 z-10 md:left-36 sm:left-0 ">
+                <Header/>
+            </div>
+
             <div className="flex flex-col w-10/12">
                 <div className="">
                     {/* for q */}
